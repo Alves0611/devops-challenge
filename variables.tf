314ddb00999449ed659860e2dc0ee7c27ef49c72
Alves0611 <g.alves0611@gmail.com>
2024-05-08 03:12:42 -0300

    adding eks related variables

--- variables.tf
+++ variables.tf
@@ -9,3 +9,25 @@ variable "service_name" {
   description = "The service name identifier"
   default     = "devops-challenge"
 }
+
+variable "cluster_version" {
+  type        = number
+  description = "eks cluster version"
+  default     = "1.29"
+}
+
+variable "node_groups" {
+  description = "node group settings"
+  type = object({
+    min_size     = number
+    max_size     = number
+    desired_size = number
+    instace_type = string
+  })
+  default = {
+    min_size     = 2
+    max_size     = 2
+    desired_size = 2
+    instace_type = "t2.micro"
+  }
+}
